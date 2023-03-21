b30f581c7a64d815d3c64bce4648509119582e91
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-03-21 17:50:18 -0300

    Inicio do curso Lógica condicional e controle de fluxo.

---
+++ Lógica condicional e controle de fluxos em Java/OperadoresLogicos.java
@@ -0,0 +1,30 @@
+public class OperadoresLogicos {
+/*
+Simbolos especiais quais são capazes de realizar comparações lógicas
+entre operandos lógicos ou expressões e , em seguida retornae um resultado
+
+tipos
+conjunção
+disjunção
+disjunção exclusica
+negação
+
+conjunção
+tabela verdade and (e)
+simmbologia &&
+
+disjunção
+tabela verdade or ((ou)
+simbologia ||
+
+disjunção excclusiva
+
+terminologia xor
+simbologia ^
+
+negação
+Terminologia
+inversão
+simbologia !
+*/
+}
